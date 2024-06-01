0affd1c0eee350d0e55f67eb384cd28b22b7342b
GDuyCats <luongduycat@gmail.com>
2024-06-01 13:18:49 +0700

    minor fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import logoImage from '../assets/Without slogan.png'
 function Header() {
     return (
@@ -9,13 +9,13 @@ function Header() {
                 <div className=" space-x-4 col-span-2 mx-auto">
                     <div className="space-x-4 hidden lg:flex ">
                         <div className='group'>
-                            <Link to="/aboutus" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f">VỀ CHÚNG TÔI</Link>
+                            <NavLink to="/aboutus" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f ">VỀ CHÚNG TÔI</NavLink>
                         </div>
                         <div className='group'>
                             <Link to="/shop" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f">MUA HÀNG</Link>
                         </div>
                         <div className='group'>
-                            <Link to="/forum" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f">DIỄN ĐÀN</Link>
+                            <Link to="/forum" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f ">DIỄN ĐÀN</Link>
                         </div>
                         <div className='group'>
                             <Link to="/contact" className="text-blue_177f9f px-3 py-2 text-lg font-medium border-b-4 border-b-transparent group-hover:border-b-blue_177f9f">LIÊN HỆ</Link>
